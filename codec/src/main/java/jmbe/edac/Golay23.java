5f1e5a69a4e3c60e097583532aa1d61530aa956a
Allan Bazinet <allan.bazinet@gmail.com>
2026-04-06 10:10:40 -0700

    Private checksums; remove dead errors variable

--- codec/src/main/java/jmbe/edac/Golay23.java
+++ codec/src/main/java/jmbe/edac/Golay23.java
@@ -27,12 +27,16 @@ public class Golay23
 {
 	public static final int MAX_CORRECTABLE_ERRORS = 3;
 	
-	public static final int[] CHECKSUMS = new int[]
+	private static final int[] CHECKSUMS = new int[]
 	{
 	    0x63A, 0x31D, 0x7B4, 0x3DA, 0x1ED, 0x6CC, 0x366, 0x1B3, 
 	    0x6E3, 0x54B, 0x49F, 0x475, 0x400, 0x200, 0x100, 0x080, 
 	    0x040, 0x020, 0x010, 0x008, 0x004, 0x002, 0x001 
 	};
+
+	private Golay23()
+	{
+	}
 
 	/**
 	 * Implements Golay(23,11,7) error detection and correction.  Returns the
@@ -60,7 +64,6 @@ public class Golay23
 
 		int index = -1;
 		int syndromeWeight = MAX_CORRECTABLE_ERRORS;
-		int errors = 0;
 		
 		while( index < 23 )
 		{
@@ -83,18 +86,11 @@ public class Golay23
 			{
 				for( int i = 0; i < 23; i++ )
 				{
-					errors = Integer.bitCount( syndrome );
-					
-					if( errors <= syndromeWeight )
+					if( Integer.bitCount( syndrome ) <= syndromeWeight )
 					{
 						copy.xor( 12, 11, syndrome );
 						
 						copy.rotateRight( i, 0, 22 );
-
-						if( index >= 0 )
-						{
-							errors ++;
-						}
 
 						int corrected = copy.getInt( 0, 22 );
 						int original = frame.getInt( startIndex, startIndex + 22 );
